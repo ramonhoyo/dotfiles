9f2c4455b62df32d916b2c962cef1e2d91dca8b2
Ramón Hoyo <rahoyo@outlook.com>
2024-10-09 09:53:36 -0400

    nvim

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,27 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader><F3>", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader><F2>", vim.cmd.Neotree)
+
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end)
+vim.keymap.set("n", "<F10>", function()
+  require("dap").continue()
+end)
+
+vim.keymap.set("n", "<F10>", function()
+  require("dap").step_over()
+end)
+vim.keymap.set("n", "<F11>", function()
+  require("dap").step_into()
+end)
+vim.keymap.set("n", "<F12>", function()
+  require("dap").step_out()
+end)
+
+vim.keymap.set("n", "<Leader>q", function()
+  require("dap").toggle_breakpoint()
+end)
