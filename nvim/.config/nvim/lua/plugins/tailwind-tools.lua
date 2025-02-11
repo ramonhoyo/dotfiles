b152ebecad1870b9a39e8a8e3d93e8e9e2dddb5f
= <=>
2025-02-11 15:31:03 -0400

    tailwind support for cva clsx

--- nvim/.config/nvim/lua/plugins/tailwind-tools.lua
+++ nvim/.config/nvim/lua/plugins/tailwind-tools.lua
@@ -8,5 +8,23 @@ return {
     "nvim-telescope/telescope.nvim", -- optional
     "neovim/nvim-lspconfig", -- optional
   },
-  opts = {}, -- your configuration
+  opts = {
+    server = {
+      settings = {
+        experimental = {
+          classRegex = {
+            "tw`([^`]*)",
+            'tw="([^"]*)',
+            'tw={"([^"}]*)',
+            "tw\\.\\w+`([^`]*)",
+            "tw\\(.*?\\)`([^`]*)",
+            { "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+            { "classnames\\(([^)]*)\\)", "'([^']*)'" },
+            { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+            { "cn\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+          },
+        },
+      },
+    },
+  },
 }
