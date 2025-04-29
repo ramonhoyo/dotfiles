e35bdea0e2c538dc83ab3bfb04dab7d500571773
= <=>
2025-04-29 09:12:25 -0400

    feat(spell): Disable spell in favor of cSpell

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.spelllang = "en_us"
-vim.opt.spell = true
+-- vim.opt.spell = true
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
